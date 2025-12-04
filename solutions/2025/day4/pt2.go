e619050d24bc1607a23b23e9827a4477c2f453aa
zFlxw <hello@flxw.dev>
2025-12-04 09:29:00 +0100

    feat: day 04

---
+++ solutions/2025/day4/pt2.go
@@ -0,0 +1,83 @@
+package day4
+
+import (
+	"advent-of-go/utils"
+	"strconv"
+	"strings"
+)
+
+func Pt2() utils.Solution {
+	return utils.Solution{
+		Year:       2025,
+		Day:        4,
+		Part:       2,
+		Calculator: pt2,
+	}
+}
+
+func pt2(input string) (string, error) {
+	lines := strings.Split(input, "\n")
+	matrix := make([][]bool, len(lines))
+	for i, line := range lines {
+		matrix[i] = make([]bool, len(line))
+		for j, char := range line {
+			matrix[i][j] = char == '@'
+		}
+	}
+
+	directions := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	totalAmount := 0
+	for {
+		neighbours := make([][]int, len(lines))
+		for i := range neighbours {
+			neighbours[i] = make([]int, len(lines[0]))
+			for j := range neighbours[i] {
+				neighbours[i][j] = -1
+			}
+		}
+
+		for i, row := range matrix {
+			for j, col := range matrix[i] {
+				if !col {
+					continue
+				}
+
+				count := 0
+
+				for _, dir := range directions {
+					newRow, newCol := i+dir[0], j+dir[1]
+					if newRow >= 0 && newRow < len(matrix) && newCol >= 0 && newCol < len(row) {
+						if matrix[newRow][newCol] {
+							count++
+						}
+					}
+				}
+
+				neighbours[i][j] = count
+			}
+		}
+
+		amount := 0
+		for row := range neighbours {
+			for col := 0; col < len(neighbours[row]); col++ {
+				val := neighbours[row][col]
+				if val >= 0 && val < 4 {
+					matrix[row][col] = false
+					amount++
+				}
+			}
+		}
+
+		totalAmount += amount
+		if amount == 0 {
+			break
+		}
+	}
+
+	return strconv.Itoa(totalAmount), nil
+}
--- solutions/2025/solutions.go
+++ solutions/2025/solutions.go
@@ -5,8 +5,9 @@ import (
 	"advent-of-go/solutions/2025/day1"
 	"advent-of-go/solutions/2025/day2"
 	"advent-of-go/solutions/2025/day3"
+	"advent-of-go/solutions/2025/day4"
 )
 
 func Solutions() []utils.Solution {
-	return []utils.Solution{day1.Pt1(), day1.Pt2(), day2.Pt1(), day2.Pt2(), day3.Pt1(), day3.Pt2()}
+	return []utils.Solution{day1.Pt1(), day1.Pt2(), day2.Pt1(), day2.Pt2(), day3.Pt1(), day3.Pt2(), day4.Pt1(), day4.Pt2()}
 }
